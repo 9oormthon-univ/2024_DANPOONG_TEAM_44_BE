d94acdd297067a04e742575ddfb0060891ed59b2
woojuice1102 <122527068+KangWooJu@users.noreply.github.com>
2024-11-18 01:45:22 +0900

    feat: default Package 생성

---
+++ ZipCock_44/src/main/java/org/danpoong/zipcock_44/ZipCock44Application.java
@@ -0,0 +1,13 @@
+package org.danpoong.zipcock_44;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZipCock44Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZipCock44Application.class, args);
+    }
+
+}
---
+++ ZipCock_44/src/test/java/org/danpoong/zipcock_44/ZipCock44ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.danpoong.zipcock_44;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZipCock44ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
