4b1ad6bbe2f4a8009065f410bda6ccaf0823d666
Seungwan Yoo <ysw99@kakao.com>
2024-11-21 13:12:25 +0900

    feat: 건축물 대장정보 조회 구현 완료

---
+++ ZipCock_44/src/main/java/org/danpoong/zipcock_44/domain/buildinginfo/controller/BuildingRegisterInfoController.java
@@ -0,0 +1,23 @@
+package org.danpoong.zipcock_44.domain.buildinginfo.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.danpoong.zipcock_44.domain.buildinginfo.dto.BuildingInfoRequestDto;
+import org.danpoong.zipcock_44.domain.buildinginfo.dto.BuildingInfoResponseDto;
+import org.danpoong.zipcock_44.domain.buildinginfo.service.BuildingRegisterInfoService;
+import org.danpoong.zipcock_44.global.common.response.ApiResponse;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+public class BuildingRegisterInfoController {
+    private final BuildingRegisterInfoService buildingRegisterInfoService;
+
+    @GetMapping("/building")
+    public ApiResponse<List<BuildingInfoResponseDto>> getBuildingInfo(@ModelAttribute BuildingInfoRequestDto req) {
+        return ApiResponse.ok(buildingRegisterInfoService.getBuildingInfoData(req));
+    }
+}
