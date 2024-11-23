9dbfcf86de58bb1755add038fd690575064123b0
Seungwan Yoo <ysw99@kakao.com>
2024-11-23 12:55:08 +0100

    fix: JWT 토큰 유효시간 변경

--- src/main/java/org/danpoong/zipcock_44/global/jwt/filter/LoginFilterImpl.java
+++ src/main/java/org/danpoong/zipcock_44/global/jwt/filter/LoginFilterImpl.java
@@ -88,7 +88,7 @@ public class LoginFilterImpl extends UsernamePasswordAuthenticationFilter{
         String role = auth.getAuthority();
 
         // Access 토큰과 Refresh 토큰 생성
-        String access = jwtUtil.createJwt("access", username, role, 600000L); // Access 토큰
+        String access = jwtUtil.createJwt("access", username, role, 3600000L); // Access 토큰
         String refresh = jwtUtil.createJwt("refresh", username, role, 86400000L); // Refresh 토큰
 
         // Refresh 토큰 저장
